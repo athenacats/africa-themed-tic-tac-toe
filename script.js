b851fd46141b38e2ddac6c76698a8d123c03f2ec
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-06 15:14:09 +0300

    Add loop for selecting players

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function Player(name, marker) {
+/* function Player(name, marker) {
   this.name = name;
   this.marker = marker;
-}
+} */
 
 const btnChoosePlayers = document.getElementById("btnChoosePlayers");
 const btnPlayerNameOne = document.getElementById("btnPlayerNameOne");
@@ -9,12 +9,28 @@ const choosePlayers = document.getElementById("choosePlayers");
 const playerNameOne = document.getElementById("playerNameOne");
 const playerNameTwo = document.getElementById("playerNameTwo");
 
-btnChoosePlayers.addEventListener("click", () => {
-  choosePlayers.style.display = "none";
-  playerNameOne.style.display = "flex";
-});
-
 btnPlayerNameOne.addEventListener("click", () => {
   playerNameOne.style.display = "none";
   playerNameTwo.style.display = "flex";
+});
+
+const numberOfPlayersRadios = document.querySelectorAll(
+  'input[name="numberOfPlayers"]'
+);
+
+btnChoosePlayers.addEventListener("click", () => {
+  for (const numberOfPlayersRadio of numberOfPlayersRadios) {
+    if (numberOfPlayersRadio.checked) {
+      if (numberOfPlayersRadio.value === "twoPlayers") {
+        choosePlayers.style.display = "none";
+        playerNameOne.style.display = "flex";
+      } else if (numberOfPlayersRadio.value === "onePlayer") {
+        choosePlayers.style.display = "none";
+      }
+    } else if (!numberOfPlayersRadio.checked) {
+      document.getElementById("error").style.display = "flex";
+      document.getElementById("error").style.color = "red";
+      document.getElementById("error").style.fontSize = "1rem";
+    }
+  }
 });
