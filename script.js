ceb8f0f74cfdd3f88e788ceb6068d94f27366642
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-06 14:18:38 +0300

    Add code that hides playerNameOne & reveals playerNameTwo forms

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ function Player(name, marker) {
 }
 
 const btnChoosePlayers = document.getElementById("btnChoosePlayers");
+const btnPlayerNameOne = document.getElementById("btnPlayerNameOne");
+const choosePlayers = document.getElementById("choosePlayers");
+const playerNameOne = document.getElementById("playerNameOne");
+const playerNameTwo = document.getElementById("playerNameTwo");
 
 btnChoosePlayers.addEventListener("click", () => {
-  const choosePlayers = document.getElementById("choosePlayers");
-  const playerNameOne = document.getElementById("playerNameOne");
-
   choosePlayers.style.display = "none";
   playerNameOne.style.display = "flex";
+});
+
+btnPlayerNameOne.addEventListener("click", () => {
+  playerNameOne.style.display = "none";
+  playerNameTwo.style.display = "flex";
 });
