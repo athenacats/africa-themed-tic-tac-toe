2bd0cd5cd8bb6aea2f3706e7c05aa5d2c59a487a
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-11 07:41:40 +0300

    UNable to figure out code, restarting

--- script.js
+++ script.js
@@ -60,5 +60,92 @@ btnPlayerNameTwo.addEventListener("click", () => {
   game.style.display = "grid";
 });
 
-/*playerNameOne.style.display = "none";
-playerNameTwo.style.display = "flex";*/
+/*const playerOneName = document.getElementById("playerOneName");
+const playerTwoName = document.getElementById("playerTwoName");
+
+function insertNames() {
+  playerOneName.innerhtml += playerOne.value;
+  playerTwoName.append(playerTwo.value);
+}*/
+
+const playerFactory = (name, symbol) => {
+  const playerturn = (grid, cell) => {
+    const index = grid.cells.findIndex(function (position) {
+      return position === cell;
+    });
+    if (grid.gridArray[index] === "") {
+      grid.render();
+      return index;
+    }
+    return null;
+  };
+  return { name, symbol, playerTurn };
+};
+
+const gridModule = (() => {
+  let gridArray = ["", "", "", "", "", "", "", "", ""];
+  const gameBoard = document.querySelector(".grid");
+  const cells = Array.from(document.querySelectorAll(".cell"));
+  let winner = null;
+
+  const render = () => {
+    gridArray.forEach((symbol, index) => {
+      cells[index].textContent = gridArray[index];
+    });
+  };
+
+  const reset = () => {
+    gridArray = ["", "", "", "", "", "", "", "", ""];
+  };
+
+  const checkWinner = () => {
+    const winningArrays = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningArray.forEach((combo) => {
+      if (
+        gridArray[combo[0]] &&
+        gridArray[combo[0]] === gridArray[combo[1]] &&
+        gridArray[combo[0]] === gridArray[combo[2]]
+      ) {
+        winner = "current";
+      }
+    });
+    return winner || (gridArray.includes("") ? null : "Tie");
+  };
+
+  return {
+    render, gameBoard, cells, gridArray,checkWinner, reset
+  }
+})();
+
+
+const gamePlay = (() => {
+  const playerOneName = document.getElementById("playerOne");
+  const playerTwoName = document.getElementById("playerTwo");
+  const resetbtn = document.querySelector('#reset');
+  let currentPlayer;
+  let playerOne;
+  let playerTwo;
+
+  const switchTurn = () => {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const gameRound = () => {
+    const grid = gridModule;
+    const gameStatus = document.querySelector('.game-status');
+    if (currentPlayer.name !== '') {
+      gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+    } else {
+      gameStatus.textContent = 'Board: ';
+    }
+})
